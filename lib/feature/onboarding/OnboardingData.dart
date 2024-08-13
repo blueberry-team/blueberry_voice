d2ad6dbf1d0c27707f2c85a4d7818993359e460c
Jungwoo <108061510+jwson-automation@users.noreply.github.com>
2024-08-13 19:33:44 +0900

    fix strings

--- lib/feature/onboarding/OnboardingData.dart
+++ lib/feature/onboarding/OnboardingData.dart
@@ -1,3 +1,5 @@
+import 'package:blueberry_flutter_template/utils/AppStrings.dart';
+
 import '../../gen/assets.gen.dart';
 
 class OnboardingData {
@@ -7,15 +9,19 @@ class OnboardingData {
 
   static final List<Map<String, String>> pageDataList = [
     {
-      title: "환영합니다!",
-      description: "우리 앱으로 최고의 경험을 해보세요.",
+      title: AppStrings.onboardingTitle,
+      description: AppStrings.onboardingDescription,
       image: Assets.images.sample
     },
     {
-      title: "연결 유지",
-      description: "사랑하는 사람들과 항상 연결되세요.",
+      title: AppStrings.onboardingTitle2,
+      description: AppStrings.onboardingDescription2,
       image: Assets.images.sample
     },
-    {title: "시작하기", description: "여정을 시작해봅시다!", image: Assets.images.sample},
+    {
+      title: AppStrings.onboardingTitle3,
+      description: AppStrings.onboardingDescription3,
+      image: Assets.images.sample
+    },
   ];
 }
